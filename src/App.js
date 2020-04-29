e7e2e363cd9c71bc4e079786832df3fe90b2bfa5
Alex Tan <alextan785@gmail.com>
2020-04-29 16:01:39 -0700

    decoupled options in navbar

--- src/App.js
+++ src/App.js
@@ -12,14 +12,29 @@ import ProjectsSection from "./components/ProjectsSection";
 // TODO: change navbar when i scroll to a location
 // TODO: Scroll should be to the offset - the height of the navbar
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.refList = this.generateSectionRefs();
+	}
+
+	generateSectionRefs = () => {
+		const refList = {};
+		for (let i = 0; i < options.length; i++) {
+			const option = options[i];
+			refList[option] = createRef();
+		}
+		return refList;
+	};
+
 	render() {
-		const aboutRef = createRef();
 		return (
 			<div className="App">
-				<NavBar aboutRef={aboutRef} />
-				<Header handleLearnMore={() => window.scrollTo(0, aboutRef.current.offsetTop)} />
+				<NavBar refList={this.refList} options={options} />
+				<Header
+					handleLearnMore={() => window.scrollTo(0, this.refList.About.current.offsetTop - 55)}
+				/>
 				<main>
-					<AboutSection refProp={aboutRef} />
+					<AboutSection refProp={this.refList.About} />
 					<ExperienceSection />
 					<ProjectsSection />
 				</main>
@@ -27,3 +42,5 @@ export default class App extends React.Component {
 		);
 	}
 }
+
+const options = ["Home", "About", "Experience", "Projects", "Resume", "Contact"];
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,38 +4,29 @@ import "../styles/NavBar.css";
 import NavOption from "./NavOption";
 
 export default class NavBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { selectedIndex: 0 };
-  }
+	constructor(props) {
+		super(props);
+		this.state = { selectedIndex: 0 };
+	}
 
-  handleClick = (index) => {
-    this.setState({ selectedIndex: index });
-    // TODO: Scroll to the right spot
-  };
+	handleClick = (index) => {
+		this.setState({ selectedIndex: index });
+		// TODO: Scroll to the right spot
+	};
 
-  render() {
-    const renderedOptions = options.map((option, index) => {
-      return (
-        <NavOption
-          key={option}
-          idx={index}
-          text={option}
-          selected={index === this.state.selectedIndex}
-          onClick={() => this.handleClick(index)}
-        />
-      );
-    });
+	render() {
+		const renderedOptions = this.props.options.map((option, index) => {
+			return (
+				<NavOption
+					key={option}
+					idx={index}
+					text={option}
+					selected={index === this.state.selectedIndex}
+					onClick={() => this.handleClick(index)}
+				/>
+			);
+		});
 
-    return <div className="navbar">{renderedOptions}</div>;
-  }
+		return <div className="navbar">{renderedOptions}</div>;
+	}
 }
-
-const options = [
-  "Home",
-  "About",
-  "Experience",
-  "Projects",
-  "Resume",
-  "Contact",
-];
