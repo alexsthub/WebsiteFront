518880381bad1b9a3c4228ea0608b128b104f3a5
Alex Tan <alextan785@gmail.com>
2020-04-30 12:20:24 -0700

    working popper tooltips

--- src/components/PortalBar.js
+++ src/components/PortalBar.js
@@ -5,16 +5,17 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import "../styles/PortalBar.css";
 import { Links } from "../constants/text";
 
-import { usePopper } from "react-popper";
+import "react-tippy/dist/tippy.css";
+import { Tooltip } from "react-tippy";
 
 // TODO: Show popper on hover
 export default class PortalBar extends React.Component {
 	render() {
 		return (
 			<div className="portal-container">
-				<ClickableIcon link={Links.github} icon={faGithub} />
-				<ClickableIcon link={Links.linkedin} icon={faLinkedin} />
-				<ClickableIcon link={Links.email} icon={faEnvelope} />
+				<ClickableIcon link={Links.github} icon={faGithub} title={"Github"} />
+				<ClickableIcon link={Links.linkedin} icon={faLinkedin} title={"LinkedIn"} />
+				<ClickableIcon link={Links.email} icon={faEnvelope} title={"Email"} />
 				<div className="bottom-bar" />
 			</div>
 		);
@@ -24,9 +25,18 @@ export default class PortalBar extends React.Component {
 class ClickableIcon extends React.Component {
 	render() {
 		return (
-			<a href={this.props.link}>
-				<FontAwesomeIcon className="icon" icon={this.props.icon} />
-			</a>
+			<Tooltip
+				arrow={true}
+				distance={25}
+				title={this.props.title}
+				position="right"
+				trigger="mouseenter"
+				theme={"light"}
+			>
+				<a href={this.props.link}>
+					<FontAwesomeIcon className="icon" icon={this.props.icon} />
+				</a>
+			</Tooltip>
 		);
 	}
 }
