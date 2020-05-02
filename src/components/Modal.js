08d153038d09943c930ab85c303854845d24df10
Alex Tan <alextan785@gmail.com>
2020-05-02 16:46:19 -0700

    modal animate in

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,29 +2,54 @@ import React from "react";
 import "../styles/Modal.css";
 
 export default class Modal extends React.Component {
-	componentDidMount() {
-		window.addEventListener("keydown", this.onEscKeyDown, false);
-		// setTimeout(() => this.setState({ fadeType: "in" }), 0);
+	constructor(props) {
+		super(props);
+		this.state = {
+			fadeType: null,
+		};
 	}
 
-	componentWillUnmount() {
+	componentDidMount = () => {
+		window.addEventListener("keydown", this.onEscKeyDown, false);
+		setTimeout(() => this.setState({ fadeType: "in" }), 0);
+	};
+
+	componentDidUpdate = (prevProps) => {
+		if (!this.props.show && prevProps.show) {
+			this.setState({ fadeType: "out" });
+		}
+	};
+
+	componentWillUnmount = () => {
 		window.removeEventListener("keydown", this.onEscKeyDown, false);
-	}
+	};
 
 	onEscKeyDown = (e) => {
 		if (e.key !== "Escape") return;
-		// this.setState({ fadeType: "out" });
-		this.handleClose();
+		this.setState({ fadeType: "out" });
 	};
 
-	handleClose = () => {
+	handleClose = (e) => {
+		e.preventDefault();
 		this.props.onClose();
 	};
 
+	transitionEnd = (e) => {
+		if (e.propertyName !== "opacity" || this.state.fadeType === "in") return;
+
+		if (this.state.fadeType === "out") {
+			this.props.onClose();
+		}
+	};
+
 	render() {
+		// console.log(this.state.fadeType);
+		// const backgroundFade =
+		// 	this.state.fadeType && this.state.fadeType === "in" ? "in-background" : null;
+		// console.log(backgroundFade);
 		return (
 			<div className={`modal-background`} onMouseDown={this.handleClose}>
-				<div className="modal">
+				<div className={`modal fade-${this.state.fadeType}`} onTransitionEnd={this.transitionEnd}>
 					<h3>JUST TESTING</h3>
 					<div onClick={this.handleClose}>x</div>
 				</div>
@@ -32,3 +57,4 @@ export default class Modal extends React.Component {
 		);
 	}
 }
+// TODO: Background fade and all fade out
