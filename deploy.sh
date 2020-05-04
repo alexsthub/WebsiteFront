edb2ddf826b3b1dd9ecf5334804506f7756a1773
Alex Tan <alextan785@gmail.com>
2020-05-04 15:12:38 -0700

    add ssl cert

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,15 @@ docker rm -f website
 
 docker pull alexsthub/website
 
+export TLSCERT=/etc/letsencrypt/live/alexst.me/fullchain.pem
+export TLSKEY=/etc/letsencrypt/live/alexst.me/privkey.pem
+
 docker run \
   -d \
   -p 443:443 -p 80:80 \
   -v /etc/letsencrypt:/etc/letsencrypt:ro \
-  -e TLSKEY="/etc/letsencrypt/live/alexst.me/privkey.pem" \
-  -e TLSCERT="/etc/letsencrypt/live/alexst/me/fullchain.pem" \
+  -e TLSKEY=$TLSKEY \
+  -e TLSCERT=$TLSCERT \
   --name website \
   alexsthub/website
 
