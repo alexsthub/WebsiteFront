c16d36b6af9cf6a529e01f0b3453fa5773a47aec
Alex Tan <alextan785@gmail.com>
2020-05-05 18:02:22 -0700

    base stars

--- src/components/header.js
+++ src/components/header.js
@@ -7,9 +7,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { HeaderText } from "../constants/text";
 
-// TODO: Add the static stars
+// TODO: How to make the stars not move at every animation?
 // TODO: Make the momentum logic
 const MAX_CIRCLES = 2;
+const NUM_STARS = 35;
+const GRAVITY = 1.5;
 export default class Header extends React.Component {
 	constructor(props) {
 		super(props);
@@ -25,11 +27,16 @@ export default class Header extends React.Component {
 		this.ctx = this.canvas.getContext("2d");
 
 		this.init();
-		this.animate();
+		// this.animate();
 	};
 
 	componentWillUnmount = () => {
 		window.removeEventListener("resize", this.onResize);
+	};
+
+	init = () => {
+		this.circleList = this.generateCircles(this.ctx);
+		this.generateBackground();
 	};
 
 	onResize = () => {
@@ -43,45 +50,65 @@ export default class Header extends React.Component {
 		const width = window.innerWidth;
 		const height = window.innerHeight;
 
-		this.genBackgroundGradient(c, width, height);
+		this.renderBackgroundGradient(c, width, height);
+		this.renderStars(c, width, height);
 
-		this.generateMountain(c, width, height, "#646973", "#202630", [
+		this.renderMountain(c, width, height, "#646973", "#202630", [
 			[-0.2, 1],
 			[0.5, 0.2],
 			[1.2, 1],
 		]);
 
 		// Second row
-		this.generateMountain(c, width, height, "#424752", "#292c36", [
+		this.renderMountain(c, width, height, "#424752", "#292c36", [
 			[-0.15, 1],
 			[0.25, 0.4],
 			[0.65, 1],
 		]);
-		this.generateMountain(c, width, height, "#424752", "#292c36", [
+		this.renderMountain(c, width, height, "#424752", "#292c36", [
 			[1.15, 1],
 			[0.75, 0.4],
 			[0.35, 1],
 		]);
 
 		// Bottom row
-		this.generateMountain(c, width, height, "#282c34", "#282c34", [
+		this.renderMountain(c, width, height, "#282c34", "#282c34", [
 			[-0.1, 1],
 			[0.2, 0.75],
 			[0.5, 1],
 		]);
-		this.generateMountain(c, width, height, "#282c34", "#282c34", [
+		this.renderMountain(c, width, height, "#282c34", "#282c34", [
 			[0.2, 1],
 			[0.5, 0.75],
 			[0.8, 1],
 		]);
-		this.generateMountain(c, width, height, "#282c34", "#282c34", [
+		this.renderMountain(c, width, height, "#282c34", "#282c34", [
 			[1.1, 1],
 			[0.8, 0.75],
 			[0.5, 1],
 		]);
 	};
 
-	generateMountain = (c, width, height, colorStart, colorEnd, points) => {
+	renderStars = (c, width, height) => {
+		c.strokeStyle = "white";
+		c.fillStyle = "white";
+		c.shadowColor = "white";
+		for (let i = 0; i < NUM_STARS; i++) {
+			const xPos = Math.random() * width;
+			const yPos = Math.random() * height;
+			const radius = Math.floor(Math.random() * 5);
+
+			c.beginPath();
+			c.shadowBlur = radius * 3;
+			c.arc(xPos, yPos, radius, 0, Math.PI * 2, false);
+			c.fill();
+			c.stroke();
+		}
+
+		c.shadowBlur = 0;
+	};
+
+	renderMountain = (c, width, height, colorStart, colorEnd, points) => {
 		const gradient = c.createLinearGradient(0, 0, 0, height);
 		gradient.addColorStop(0, colorStart);
 		gradient.addColorStop(1, colorEnd);
@@ -98,7 +125,7 @@ export default class Header extends React.Component {
 		c.fill();
 	};
 
-	genBackgroundGradient = (c, width, height) => {
+	renderBackgroundGradient = (c, width, height) => {
 		let gradient = c.createLinearGradient(50, 0, 0, height);
 		gradient.addColorStop(0, "#192233");
 		gradient.addColorStop(1, "rgba(69, 77, 94, 1)");
@@ -114,7 +141,7 @@ export default class Header extends React.Component {
 			const x = Math.random() * (this.canvas.width - radius * 2) + radius;
 			const y = 60;
 			const dx = (Math.random() - 0.5) * 8;
-			const dy = 5;
+			const dy = 8;
 
 			const circle = new Circle(x, y, dx, dy, radius, this.canvas.width, this.canvas.height, ctx);
 			circleList.push(circle);
@@ -123,17 +150,12 @@ export default class Header extends React.Component {
 		return circleList;
 	};
 
-	init = () => {
-		this.circleList = this.generateCircles(this.ctx);
-		this.generateBackground();
-	};
-
 	animate = () => {
 		requestAnimationFrame(this.animate);
 		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 		this.generateBackground();
 		this.circleList.forEach((c) => {
-			c.update();
+			// c.update();
 		});
 	};
 
@@ -187,6 +209,7 @@ class Circle {
 	update() {
 		if (this.x + this.radius > this.maxWidth || this.x - this.radius < 0) this.dx = -this.dx;
 		if (this.y + this.radius > this.maxHeight || this.y - this.radius < 0) this.dy = -this.dy;
+		this.dy = this.dy + GRAVITY;
 		this.y += this.dy;
 		this.x += this.dx;
 
