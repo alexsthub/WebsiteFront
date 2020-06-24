b9bacfdd6dabc221b770571e6c1e5046b4b538b0
Alex Tan <alextan785@gmail.com>
2020-06-24 12:35:21 -0700

    Updates

---
+++ precache-manifest.1d50580d73c85c614f24a3d1676fe42a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17a9d7c9f905dd83ce3d9cdb0fe7ea0",
+    "url": "/WebsiteFront/index.html"
+  },
+  {
+    "revision": "3a8b0d78a3b20b78d1dd",
+    "url": "/WebsiteFront/static/css/2.e22355b1.chunk.css"
+  },
+  {
+    "revision": "1132a58769142b5499e5",
+    "url": "/WebsiteFront/static/css/main.32fd8502.chunk.css"
+  },
+  {
+    "revision": "3a8b0d78a3b20b78d1dd",
+    "url": "/WebsiteFront/static/js/2.d1e127bb.chunk.js"
+  },
+  {
+    "revision": "95aed6a00765aeb803e861408bf7612d",
+    "url": "/WebsiteFront/static/js/2.d1e127bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1132a58769142b5499e5",
+    "url": "/WebsiteFront/static/js/main.90956930.chunk.js"
+  },
+  {
+    "revision": "3e9f131eb2e060c5f749",
+    "url": "/WebsiteFront/static/js/runtime-main.0d66e94b.js"
+  },
+  {
+    "revision": "2e26cbdd67a14f4cf52aedf31eb8fbfb",
+    "url": "/WebsiteFront/static/media/resume.2e26cbdd.pdf"
+  }
+]);
